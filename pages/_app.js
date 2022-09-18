4968cfaf9078ce3beef6ea116d39c25eff7a8fc9
DanRatherNot <34725981+dandc11@users.noreply.github.com>
2022-09-18 16:03:00 -0500

    Revert "Feat/css reset"

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
-import '../styles/globals.css';
-import 'the-new-css-reset/css/reset.css';
-import Layout from '../components/Layout';
+import '../styles/globals.css'
+import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
     return (
         <Layout test={'some words'}>
             <Component {...pageProps} />
         </Layout>
-    );
+    )
 }
 
-export default MyApp;
+export default MyApp
