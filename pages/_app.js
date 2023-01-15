4c7762b1faf7e8c4a0aa2a8e47221c9a5ec50fe2
DanRatherNot <34725981+dandc11@users.noreply.github.com>
2023-01-15 15:02:37 -0600

    Update to use Next Font

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,31 @@
 import './../styles/global.css';
+import { Playfair_Display, Raleway } from '@next/font/google';
 import Layout from '../components/Layout';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
+const PLAYFAIR_DISPLAY = Playfair_Display({
+    variable: '--font-playfair-display',
+    style: ['normal', 'italic'],
+    subsets: ['latin'],
+});
+
+const RALEWAY = Raleway({
+    variable: '--font-raleway',
+    style: ['normal', 'italic'],
+    subsets: ['latin'],
+});
+
+function OzarkedgeApp({ Component, pageProps }) {
     return (
-        <>
+        <div className={`${PLAYFAIR_DISPLAY.variable} ${RALEWAY.variable}`}>
+            <Head>
+                <title>Ozarkedge Wildflowers</title>
+            </Head>
             <Layout>
                 <Component {...pageProps} />
             </Layout>
-        </>
+        </div>
     );
 }
 
-export default MyApp;
+export default OzarkedgeApp;
