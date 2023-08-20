ce64b94ce5ae245efb330dd58dd504f8377c537e
DanRatherNot <34725981+dandc11@users.noreply.github.com>
2023-08-20 17:14:05 -0500

    Vercel build fixes

--- pages/season/[slug].js
+++ pages/season/[slug].js
@@ -1,54 +1,49 @@
-import React from 'react';
-
-import { sanityClient } from '@lib/sanity.client';
-import { GET_ALL_SEASON_PATHS_QUERY } from '@lib/queries';
-import PlantName from 'components/PlantName';
+import React from 'react'
+import { getClient } from '../../lib/sanity.client'
+import { GET_ALL_SEASON_PATHS_QUERY } from '../../lib/queries'
+import PlantName from 'components/PlantName'
 
 const SeasonPage = ({ plantPageData }) => {
-    // const {
-    //     conservationStatus,
-    //     description,
-    //     floweringColor,
-    //     floweringMonths,
-    //     floweringSeason,
-    //     growingNearbyText,
-    //     habitat,
-    //     images,
-    //     plantName,
-    //     previewImage,
-    //     tidbits,
-    // } = plantPageData;
-    return (
-        <div>
-            {/* <PlantName plantName={plantName}></PlantName> */}
-        </div>
-    );
-};
+  // const {
+  //     conservationStatus,
+  //     description,
+  //     floweringColor,
+  //     floweringMonths,
+  //     floweringSeason,
+  //     growingNearbyText,
+  //     habitat,
+  //     images,
+  //     plantName,
+  //     previewImage,
+  //     tidbits,
+  // } = plantPageData;
+  return <div>{/* <PlantName plantName={plantName}></PlantName> */}</div>
+}
 
 export async function getStaticPaths() {
-    const plantPagePaths = await sanityClient.fetch(GET_ALL_SEASON_PATHS_QUERY);
-const paths = plantPagePaths.map((slug) => ({
-        params: { slug },
-    }));
-    return {
-        paths,
-        fallback: true,
-    };
+  const plantPagePaths = await sanityClient.fetch(GET_ALL_SEASON_PATHS_QUERY)
+  const paths = plantPagePaths.map((slug) => ({
+    params: { slug },
+  }))
+  return {
+    paths,
+    fallback: true,
+  }
 }
 
 export async function getStaticProps(context) {
-    const { slug = '' } = context.params;
-    const plantPageData = await sanityClient.fetch(
-        `
+  const { slug = '' } = context.params
+  const plantPageData = await sanityClient.fetch(
+    `
         *[_type == "season" && slug.current == $slug][0] {...}
         `,
-        { slug }
-    );
-    return {
-        props: {
-            plantPageData,
-        },
-    };
+    { slug }
+  )
+  return {
+    props: {
+      plantPageData,
+    },
+  }
 }
 
-export default SeasonPage;
+export default SeasonPage
