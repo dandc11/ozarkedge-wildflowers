be4c527d89d0c8b7d3af36d6ba58db1a8a4fe340
DanRatherNot <34725981+dandc11@users.noreply.github.com>
2024-05-26 12:43:30 -0500

    Refactor portable text file

---
+++ schemas/objects/imageCollection.js
@@ -0,0 +1,36 @@
+import { defineType } from 'sanity'
+import { ImageCollectionPreview } from '../components/ImageCollectionPreview'
+
+export default defineType({
+  name: 'imageCollection',
+  type: 'object',
+  title: 'Image Collection',
+  fields: [
+    {
+      name: 'imageCollection',
+      description:
+        'If you add images here, they will display within the text block together as part of a collection. You can add as many images as you like, and add captions indicating any connections between the images.',
+      title: 'Image',
+      type: 'array',
+      of: [
+        {
+          type: 'figure',
+          title: 'Image',
+          description: 'Add an image to the page.',
+          options: { hotspot: true },
+        },
+      ],
+    },
+  ],
+  components: { preview: ImageCollectionPreview },
+  preview: {
+    select: {
+      collection: 'imageCollection',
+    },
+    prepare(selection) {
+      return {
+        ...selection,
+      }
+    },
+  },
+})
