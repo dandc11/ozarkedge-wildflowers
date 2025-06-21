987482d8bb7b2fd8ec14cc82b68805c4b5e56e99
DanRatherNot <34725981+dandc11@users.noreply.github.com>
2025-06-21 09:18:18 -0500

    chore: add @next/bundle-analyzer dependency to package.json

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     domains: ['cdn.sanity.io'],
     loader: 'custom',
   },
   productionBrowserSourceMaps: true,
 }
+
+module.exports = withBundleAnalyzer(nextConfig)
